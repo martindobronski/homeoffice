84641de5d29752c78aa3db76fa7c19b46a57f397
Martin <infomartin@arcor.de>
2026-09-04 22:01:53 +0200

    Punkt: größer + weißer Rand + vertical-align: super für mehr Abstand zur Zahl; Cache-Buster 2.55/1.223, SW v2.66

--- sw.js
+++ sw.js
@@ -7,13 +7,13 @@
    - übrige gleiche-Origin-Assets: stale-while-revalidate (Cache zuerst, im Hintergrund aktualisieren)
    Bei einem Release CACHE_NAME zusammen mit den Cache-Bustern in index.html erhöhen. */
 
-const CACHE_NAME = 'homeoffice-v2.65';
+const CACHE_NAME = 'homeoffice-v2.66';
 
 const PRECACHE = [
     './',
     'index.html',
-    'style.css?v=2.54',
-    'app.js?v=1.222',
+    'style.css?v=2.55',
+    'app.js?v=1.223',
     'tabler-icons.min.css?v=3',
     'fonts/tabler-icons.woff2',
     'fonts/tabler-icons-filled.woff2',
